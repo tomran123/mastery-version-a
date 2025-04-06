3a91abaf95a88d24bd06e395e6e8fc3b736dbea0
Cunzhou Ran <rancunzhou123@gmail.com>
2025-04-05 20:07:00 -0500

    complete

--- calculator.c
+++ calculator.c
@@ -12,10 +12,16 @@ int calculate_result(struct Reader *reader) {
             // this is an operation:
             struct Token *op = head;
             struct Token *val_a = head->next;
+
+            if (!val_a) {
+                reader->had_error = true;
+                return -1;
+            }
             struct Token *val_b = head->next->next;
 
+
             struct Token *new_token = malloc(sizeof(struct Token));
-            if (!new_token) {
+            if (!new_token || !val_b) {
                 reader->had_error = true;
                 return -1;
             }
